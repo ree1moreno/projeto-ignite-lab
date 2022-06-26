2ddafd36263279d4bdd6c17f58971173344cbe94
Renato Moreno <renats.mc@gmail.com>
2022-06-26 11:08:25 -0300

    player de video e descricao + roteamento das aulas

---
+++ event-platform/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import { Icon } from "./Icon";
+
+export function Footer() {
+  return (
+    <footer className="sticky top-[100vh] bg-gray-900 p-6 flex justify-between items-center border-t border-gray-300 w-full max-w-[1100px] mx-auto">
+      <div className="flex items-center gap-6">
+        <Icon />
+        <span className="text-gray-300 text-sm">
+          Rocketseat - Todos os direitos reservados
+        </span>
+      </div>
+      <p className="text-gray-300 text-sm">
+        Políticas de privacidade
+      </p>
+    </footer>
+  )
+}
